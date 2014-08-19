87aaf5b1f1b2c7e821b8b40f21a1563dc4422621
Vde <deleplace.v@sfeir.com>
2014-08-19 10:18:17 +0200

    CSS

--- src/gotraps/static/default/js/gotraps.js
+++ src/gotraps/static/default/js/gotraps.js
@@ -1,1 +1,11 @@
-/* todo */
+(function($) {
+	
+	$("#title-stdout").click(function() {
+		$("#stdout").collapse('toggle');
+	});
+
+	$("#title-discussion").click(function() {
+		$("#discussion").collapse('toggle');
+	});
+	
+})(jQuery);
